62c70997f4293e508965b63b8104d4e6880b52b6
Matt Bernhardt <matt.j.bernhardt@gmail.com>
2019-12-12 01:13:36 -0500

    Finishes refactoring

--- day05/computer.py
+++ day05/computer.py
@@ -12,6 +12,15 @@ class computer():
 		self.instruction = -1
 		print('  ...boot complete\n')
 
+	def execute(self):
+		# This kicks off the execution of the commands in memory.
+		self.showStatus()
+		while self.instruction != 99:
+			self.op()
+			print('--- Next execution ---')
+		print('--- Final status ---')
+		self.showStatus()
+
 	def load(self, filename):
 		# This reads in a file of instructions, and performs initial setup.
 		print('Loading ' + str(filename) + '...')
@@ -22,9 +31,53 @@ class computer():
 		self.instruction = self.memory[self.position]
 		print('  ...load complete\n')
 
+	def op(self):
+		# Lookup instruction
+		self.instruction = self.memory[self.position]
+		# Quick status check
+		print('Position: ' + str(self.position))
+		print('Executing instruction: ' + str(self.instruction))		
+		operations = {
+			1: self.opAdd,
+			2: self.opMultiply,
+			99: self.opEnd
+		}
+		func = operations.get(
+			self.instruction,
+			lambda: 'Invalid operation'
+		)
+		return func()
+
+	def opAdd(self):
+		print('Adding (instruction ' + str(self.instruction) + ')')
+		print('')
+		one = self.memory[self.memory[self.position+1]]
+		two = self.memory[self.memory[self.position+2]]
+		result = one + two
+		print('Adding ' + str(one) + ' and ' + str(two) + ' to get ' + str(result))
+		print('Storing at position ' + str(self.memory[self.position+3]))
+		self.memory[self.memory[self.position+3]] = result
+		self.position = self.position + 4
+
+	def opMultiply(self):
+		print('Multipling (instruction ' + str(self.instruction) + ')')
+		print('')
+		one = self.memory[self.memory[self.position+1]]
+		two = self.memory[self.memory[self.position+2]]
+		result = one * two
+		print('Adding ' + str(one) + ' and ' + str(two) + ' to get ' + str(result))
+		print('Storing at position ' + str(self.memory[self.position+3]))
+		self.memory[self.memory[self.position+3]] = result
+		self.position = self.position + 4
+
+	def opEnd(self):
+		print('Ending')
+		print('')
+
 	def showStatus(self):
 		# This prints out the state of defined variables and whatnot.
 		print('Memory:')
 		print(str(self.memory))
 		print('Position:    ' + str(self.position))
 		print('Instruction: ' + str(self.instruction))
+		print('')
--- day05/p5-1.py
+++ day05/p5-1.py
@@ -2,78 +2,8 @@
 import sys
 from computer import computer
 
-def lookupOp(code, position, data):
-	status(code, position, data)
-	opList={
-		1:opAdd,
-		2:opMultiply,
-		99:opEnd
-	}
-	func = opList.get(code, lambda:'Invalid operation')
-	return func(code, position, data)
-
-def opAdd(code, position, data):
-	one = data[data[position+1]]
-	two = data[data[position+2]]
-	result = one + two
-	print('Adding ' + str(one) + ' and ' + str(two) + ' to get ' + str(result))
-	print('Storing at position ' + str(data[position+3]))
-	data[data[position+3]] = result
-	code = data[position+4]
-	position = position + 4
-	return code, position, data
-
-def opMultiply(code, position, data):
-	one = data[data[position+1]]
-	two = data[data[position+2]]
-	result = one * two
-	print('Multiplying ' + str(one) + ' and ' + str(two) + ' to get ' + str(result))
-	print('Storing at position ' + str(data[position+3]))
-	data[data[position+3]] = result
-	code = data[position+4]
-	position = position + 4
-	return code, position, data
-
-def opEnd(code, position, data):
-	print('Ending')
-	return code, position, data
-
-def readInputs(filename):
-	opcodes = []
-	with open(filename) as file:
-		for line in file:
-			opcodes = line.split(',')
-	file.closed
-	return [int(foo) for foo in opcodes]
-
-def status(code, position, data):
-	print('Data: ' + str(data))
-	print('Pos:  ' + str(position))
-	print('Code: ' + str(data[position]))
-	print('')
-
-def holding():
-	intCodes = readInputs(sys.argv[1])
-	thisPos = 0 #
-	thisCode = intCodes[thisPos]
-	print('')
-
-	print('Initial condition:')
-	status(thisCode, thisPos, intCodes)
-	print('--- Beginning execution ---')
-	print('')
-
-	while thisCode != 99:
-		thisCode, thisPos, intCodes = lookupOp(thisCode, thisPos, intCodes)
-
-		print('')
-		print('--- Next execution ---')
-		print('')
-
-	print('--- Final status ---')
-	status(thisCode, thisPos, intCodes)
-
 if __name__ == "__main__":
 	c = computer()
 	c.load(sys.argv[1])
 	c.showStatus()
+	c.execute()
