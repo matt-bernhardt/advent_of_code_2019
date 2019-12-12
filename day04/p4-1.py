2ef460bc115fc620f51f9bf463fbf9ba3a2d2527
Matt Bernhardt <matt.j.bernhardt@gmail.com>
2019-12-11 23:29:11 -0500

    Combine scripts for part 1 and part 2

--- day04/p4-1.py
+++ day04/p4-1.py
@@ -1,5 +1,36 @@
 #!/usr/bin/python3
 import sys
+import copy
+
+def checkDecrease(digits):
+	# Check that from left to right, digits never decrease
+	decrease = False
+	for j in range(len(digits) - 1):
+		if digits[j] > digits[j+1]:
+			decrease = True
+	return decrease
+
+def checkDouble(digits):
+	# Check that two adjacent digits are the same.
+	double = False
+	for j in range(len(digits) - 1):
+		if digits[j] == digits[j+1]:
+			double = True
+	return double
+
+def checkDoubleOnly(digits):
+	# Check that two adjacent digits are the same.
+	# We append a letter to the list because we need to exclude three
+	# consecutive digits, so we need j, j+1, and j+2 - so j+2 needs to exist.
+	# We manipulate a copy of the original list.
+	test = copy.deepcopy(digits)
+	test.insert(0,'a')
+	test.append('z')
+	double = False
+	for j in range(1, len(test) - 2):
+		if (test[j] == test[j+1]) and (test[j+1] != test[j+2]) and (test[j-1] != test[j]):
+			double = True
+	return double
 
 def readInputs(filename):
 	ranges = []
@@ -10,6 +41,12 @@ def readInputs(filename):
 	return ranges
 
 if __name__ == "__main__":
+	# Identify what puzzle part we're solving
+	puzzlePart = "one"
+	if len(sys.argv) == 3 and sys.argv[2] == "part2":
+		puzzlePart = "two"
+	print('Solving part ' + str(puzzlePart) + '\n')
+	# Define range of numbers to check
 	print('Reading range')
 	ranges = readInputs(sys.argv[1])
 	minimum = int(ranges[0])
@@ -30,28 +67,21 @@ if __name__ == "__main__":
 		# (this is guaranteed by how we set up the loop)
 
 		# Two adjacent digits are the same
-		double = False
-		for j in range(len(digits) - 1):
-			if digits[j] == digits[j+1]:
-				double = True
-		if not double:
-			#  print('No doubled digits\n')
-			continue
+		if puzzlePart == "two":
+			if checkDoubleOnly(digits) != True:
+				continue
+		else:
+			if checkDouble(digits) != True:
+				continue
 		# print('Doubled digits, we continue')
 
 		# From left to right, digits never decrease
-		decrease = False
-		for j in range(len(digits) - 1):
-			if digits[j] > digits[j+1]:
-				decrease = True
-		if decrease:
-			# print('Digits decrease \n')
+		if checkDecrease(digits):
 			continue
 		# print('Digits do not decrease, we have a candidate')
 
 		candidates.append(test)
-		# print('')
 
 	print(str(len(candidates)) + ' possible passwords')
-	print(str(candidates))
+	# print(str(candidates))
 
